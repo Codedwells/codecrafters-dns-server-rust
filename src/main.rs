ba2fdfb2f91d919d478bc24ecc87d2a215995783
codedwells <wells.mail.13@gmail.com>
2023-11-26 12:33:39 +0300

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-// Uncomment this block to pass the first stage
  use std::net::UdpSocket;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
 
-   //  Uncomment this block to pass the first stage
+
+fn main() {
+
      let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
      let mut buf = [0; 512];
 
