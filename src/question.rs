c23fc8d2ef4fbe9da3a8e629bc060a1da0c5cb49
codedwells <wells.mail.13@gmail.com>
2023-12-05 15:51:36 +0300

    fix(question): not matching quesestion type

--- src/question.rs
+++ src/question.rs
@@ -1,5 +1,5 @@
-use byteorder::{BigEndian, ByteOrder, ReadBytesExt};
-use std::io::Cursor;
+use byteorder::{BigEndian, ByteOrder};
+use std::str::from_utf8;
 
 #[allow(dead_code)]
 #[derive(Copy, Clone)]
@@ -59,38 +59,32 @@ impl DNSQuestion {
         // 12 bytes are reserved for the header
         let mut offset = 12;
 
+        // The dns question comes in compressed format -> We need to decompress it
+
         let mut domain_name = Vec::new();
 
         loop {
             let label_length = buf[offset] as usize;
+            offset += 1;
 
             if label_length == 0 {
                 break;
             }
-
-            if (label_length & 0b1100_0000) == 0b1100_0000 {
-                // Compression pointer
-                let mut cursor = Cursor::new(&buf[offset..offset + 2]);
-                let pointer = (cursor.read_u16::<BigEndian>().unwrap() & 0x3FFF) as usize;
-
-                // Follow the pointer and continue parsing
-                offset = pointer;
-                continue;
-            }
-
-            offset += 1;
 
             let label = String::from_utf8_lossy(&buf[offset..offset + label_length]);
             domain_name.push(label.to_string());
             offset += label_length;
         }
 
+        let (domain_name, new_offset) = process_compressed_name(buf, offset);
+        offset = new_offset;
+
         let query_type = BigEndian::read_u16(&buf[offset..offset + 2]);
         offset += 2;
         let query_class = BigEndian::read_u16(&buf[offset..offset + 2]);
 
         DNSQuestion {
-            domain_name,
+            domain_name: domain_name.split('.').map(|s| s.to_string()).collect(),
             query_type: match query_type {
                 1 => DNSQueryType::A,
                 2 => DNSQueryType::NS,
@@ -118,6 +112,34 @@ impl DNSQuestion {
             },
         }
     }
+}
+
+fn process_compressed_name(buffer: &[u8], position: usize) -> (String, usize) {
+    let mut offset = position;
+    let mut name = String::new();
+    loop {
+        let length = buffer[offset] as usize;
+        if length == 0 {
+            break;
+        }
+        if !name.is_empty() {
+            name.push('.');
+        }
+        if length & 0xC0 == 0xC0 {
+            let next = u16::from_be_bytes(buffer[offset..offset + 2].try_into().unwrap()) & 0x3FFF;
+            let (next_name, _) = process_compressed_name(buffer, next as usize);
+            name.push_str(&next_name);
+            offset += 1;
+            break;
+        }
+        let next = match from_utf8(&buffer[offset + 1..offset + 1 + length]) {
+            Ok(s) => s,
+            Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+        };
+        name.push_str(next);
+        offset += length + 1;
+    }
+    (name, offset + 1)
 }
 
 fn split_u16_to_u8(input: u16) -> [u8; 2] {
